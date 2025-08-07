5a0b61a34e74cbeb16c832c9485d0dfd7c7de919
swapnilg45 <swapnil.ghadi066@gmail.com>
2025-08-07 10:35:42 +0530

    add:short url

--- bhaskaragro/config/funnel.py
+++ bhaskaragro/config/funnel.py
@@ -1,47 +1,9 @@
-# import frappe
-
-# @frappe.whitelist(allow_guest=True)
-# def send_pdf_url(variables=None):
-#     try:
-#         if not variables or not variables.get("name"):
-#             frappe.throw("Document reference not provided in variables")
-
-#         so_name = frappe.get_doc("Sales Order", variables["name"])
-
-#         if not so_name:
-#             return {"error": "Missing Sales Order name"}
-
-#         # Base site URL
-#         base_url = frappe.utils.get_url()
-
-#         # Proper query parameters for the print URL
-#         params = urlencode({
-#             "doctype": "Sales Order",
-#             "name": so_name.name,
-#             "format": "Sales Order Confirmation Format",
-#             "no_letterhead": 0
-#         })
-
-#         # Full PDF link
-#         pdf_url = f"{base_url}/print?{params}"
-
-#         variables["pdf_url"] = pdf_url
-
-#         return variables
-
-#     except Exception:
-#         frappe.log_error(
-#             message=frappe.get_traceback(),
-#             title="Error in send_pdf_url"
-#         )
-#         return {"error": "Failed to generate PDF URL"}
-
 import frappe
 import requests
 from urllib.parse import urlencode
 
 @frappe.whitelist(allow_guest=True)
-def send_pdf_url(variables):
+def send_pdf_url(variables=None):
     doc = variables.get("doc")
 
     so_name = frappe.get_doc("Sales Order", doc.name)
