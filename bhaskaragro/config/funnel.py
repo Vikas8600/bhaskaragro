1814bef49460c6a99d0c63b77a6d765e07982ed4
Pragati Dike <pragati@mail.hybrowlabs.com>
2025-08-07 18:57:39 +0530

    |bhaskaragro|short pdf url|

--- bhaskaragro/config/funnel.py
+++ bhaskaragro/config/funnel.py
@@ -17,6 +17,38 @@ def send_pdf_url(variables=None):
         "name": so_name.name,
         "format": "Sales Order Confirmation Format",
         "no_letterhead": 0
+        "letterhead": "Bhaskar Agro Bellary New",
+    })
+
+    pdf_url = f"{base_url}/api/method/frappe.utils.print_format.download_pdf?{params}"
+
+    # Use TinyURL to shorten the link
+    tinyurl_api = "https://tinyurl.com/api-create.php"
+    response = requests.get(tinyurl_api, params={"url": pdf_url})
+
+    short_url = response.text if response.status_code == 200 else pdf_url
+
+    variables["pdf_url"] = short_url
+
+
+
+
+@frappe.whitelist(allow_guest=True)
+def send_pdf_url(variables=None):
+    doc = variables.get("doc")
+
+    si_name = frappe.get_doc("Sales Invoice", doc.name)
+    if not si_name:
+        return {"error": "Missing Sales Invoice name"}
+
+    base_url = frappe.utils.get_url()
+
+    params = urlencode({
+        "doctype": "Sales Invoice",
+        "name": si_name.name,
+        "format": "Sales Order Confirmation Format",
+        "no_letterhead": 0
+        "letterhead": "Bhaskar Agro Bellary New",
     })
 
     pdf_url = f"{base_url}/api/method/frappe.utils.print_format.download_pdf?{params}"
