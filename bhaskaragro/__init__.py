dc4950005414913ba04bc02f4704f23b30944efc
swapnilg45 <swapnil.ghadi066@gmail.com>
2026-03-04 17:22:30 +0530

    fix: import error

--- bhaskaragro/__init__.py
+++ bhaskaragro/__init__.py
@@ -1,8 +1,11 @@
 
 __version__ = '0.0.1'
 
-import erpnext.accounts.report.general_ledger.general_ledger as gl_module
-from bhaskaragro.overrides.general_ledger import custom_execute
+try:
+	import erpnext.accounts.report.general_ledger.general_ledger as gl_module
+	from bhaskaragro.overrides.general_ledger import custom_execute
 
-gl_module.execute = custom_execute
+	gl_module.execute = custom_execute
+except ImportError:
+	pass
 
